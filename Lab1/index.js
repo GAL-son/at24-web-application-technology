172de845bc7e61d070ab646605d94118c33b70f1
GAL_son <pancerek1@gmail.com>
2024-02-28 17:34:49 +0100

    Better named service

--- Lab1/index.js
+++ Lab1/index.js
@@ -1,5 +1,5 @@
 let service = require('./service');
 
-service.isOdd([1,2,3,4,5,6,7,7,8,9,12]).forEach(number => {
+service.getOdd([1,2,3,4,5,6,7,7,8,9,12,-2,-3]).forEach(number => {
    console.log(number); 
 });
--- Lab1/service.js
+++ Lab1/service.js
@@ -1,8 +1,8 @@
 // IDK why in exc. function is named isEven when only odd numbers are left ¯\_(ツ)_/¯
-function isOdd(arr) {
+function getOdd(arr) {
     return arr.filter((a) => {return a%2 != 0});
 }
 
 module.exports = {
-    isEven: isOdd
+    getOdd: getOdd,
 }
