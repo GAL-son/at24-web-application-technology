42661f5e23b5f3b9b53f97039ab805bfb0b1a8d8
GAL_son <pancerek1@gmail.com>
2024-02-28 16:45:40 +0100

    Exc. 3

--- Lab1/script.js
+++ Lab1/script.js
@@ -7,3 +7,22 @@ console.log(arr);
 arr.pop();
 console.log(arr);
 
+// Exc.3 
+function insertToArray(arr, end, value) {
+    if(![0,1].includes(end)) {
+        throw "Invalid end value: Must be 1 or 0 but given " + end;
+    }
+
+    if(!end) {
+        arr.push(value);
+    } else {
+        arr.unshift(value);
+    }
+
+    return arr;
+}
+
+console.log(insertToArray(arr, 1, 3));
+console.log(insertToArray(arr, 0, 8));
+// console.log(insertToArray(arr, 2, 8)); // Should throw error
+
