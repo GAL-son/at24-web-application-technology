98e92ba579b43828551050036d8b56777bf61931
GAL_son <pancerek1@gmail.com>
2024-02-28 17:11:57 +0100

    Exc. 7

--- Lab1/script.js
+++ Lab1/script.js
@@ -42,3 +42,37 @@ let numbers = "1.2.3.4.5.6.7.8.9";
 
 console.log(squareNumbers(numbers, '.'));
 
+// Exc. 5
+const poleKwadratu = (a) => {
+    return a*a;
+}
+
+console.log(poleKwadratu(5));
+console.log(poleKwadratu(50));
+
+// Exc. 6
+function randomItem(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+const students = ["Olek", "Janek", "Stefan", "Tymek", "Sławek"];
+console.log(randomItem(students));
+
+// Exc. 7
+function randomNumberTimeout(count, waitTime) {
+    if (count == 0) return;
+
+    let timeout = setTimeout(() => {
+        console.log(Math.floor(Math.random() * 10));
+        randomNumberTimeout(count-1, waitTime);
+    }, waitTime);
+}
+
+randomNumberTimeout(10, 500);
+
+function simulateServerConneciton(name) {
+    setInterval(() => {console.log("Witaj "+name);}, 3000);
+}
+setTimeout(() => {
+    simulateServerConneciton("Pawel")
+}, 5000);
