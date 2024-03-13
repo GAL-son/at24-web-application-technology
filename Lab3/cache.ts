a064f5b6d87b366d5a2300f0b0b69d0387c6d93c
GAL_son <pancerek1@gmail.com>
2024-03-13 21:43:39 +0100

    Exc. 5

---
+++ Lab3/cache.ts
@@ -0,0 +1,37 @@
+// Exc. 5
+
+type CacheArr<T> = {
+    [key: string]: T;
+};
+
+function linkCache<T>(fn: (a: T, b: T) => T, cache: CacheArr<T>) {
+    return (a: T, b: T): T => {
+        let newKey: string = "$" + a + "_$" + b;
+
+        if (!(newKey in cache)) {
+            console.log("New result saved to cache");
+            let result: T = fn(a, b);
+            cache[newKey] = result;
+        } else {
+            console.log("Result from cache", );
+        }
+        
+        return cache[newKey];
+    }
+}
+
+// Testing
+
+let addCache: CacheArr<number> = {};
+
+function add(a: number, b: number) {
+    return a + b;
+}
+
+let cachedAdd = linkCache<number>(add, addCache);
+
+console.log(cachedAdd(1,2));
+console.log(cachedAdd(2,2));
+console.log(cachedAdd(1,2));
+console.log(cachedAdd(1,1));
+
