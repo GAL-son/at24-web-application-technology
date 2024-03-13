ea9bd319258403fb9819074086fd9fa3c96eb1b1
GAL_son <pancerek1@gmail.com>
2024-03-13 21:05:22 +0100

    Exc. 3 - Testing

--- Lab3/arrays.ts
+++ Lab3/arrays.ts
@@ -1,3 +1,9 @@
+// Exc. 3
 function mergeArrays<T, U>(array1: Array<T>, array2: Array<U>) : Array<T|U> {
     return [...array1, ...array2];
 }
+
+let arr1 : Array<number> = [1,2,3];
+let arr2 : Array<string> = ["abc", "cde", "fgh"];
+
+console.log(mergeArrays(arr1, arr2));
