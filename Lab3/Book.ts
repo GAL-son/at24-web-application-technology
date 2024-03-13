07f4fa65fab18618e420df9d1e8d6e6394cb85c0
GAL_son <pancerek1@gmail.com>
2024-03-13 21:01:12 +0100

    Exc. 2 - Testing

--- Lab3/Book.ts
+++ Lab3/Book.ts
@@ -13,3 +13,19 @@ function sumYears(books: Array<Book>) {
 
     return sum;
 }
+
+class TestBook implements Book {
+    title: string;
+    author: string;
+    publicationYear: number;
+
+    constructor(title: string, publicationYear: number) {
+        this.title = title;
+        this.publicationYear = publicationYear;
+    }
+}
+
+let bookA : Book = new TestBook("BookA", 1994);
+let bookB : Book = new TestBook("BookB", 2006);
+
+console.log("bookA[" + bookA.publicationYear + "] + bookB[" + bookB.publicationYear + "] = " + sumYears([bookA, bookB]));
