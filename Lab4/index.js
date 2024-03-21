43ad888f30a0ba6128c3e141e251983524e8d554
GAL_son <pancerek1@gmail.com>
2024-03-21 17:16:58 +0100

    Exc. 5.3

--- Lab4/index.js
+++ Lab4/index.js
@@ -4,6 +4,7 @@ const questions = require('./questions').questions;
 const port = require('./config').port;
 
 const app = express();
+app.use(express.json());
 
 app.get('/api/questions', (request, response) => {
     response.send(questions);
@@ -16,6 +17,32 @@ app.get('/api/questions/:id', (request, response) => {
     } else {
         response.status(404).send("not found");
     }
+})
+
+// Used /api/questions path because /api/questions/:id doesen't sound right.
+// I would use this path for PUT to modify the questions or sth.
+app.post('/api/questions', (request, response) => {
+    const newQuestion = request.body;
+
+    const invalidRequest = (message) => {
+        response.status(400).send(message);
+    }
+
+    const questionFields = ["category","type","difficulty","question", "correct_answer","answers"];
+    questionFields.forEach(field => {
+        if(!Object.hasOwn(newQuestion, field)) {
+            invalidRequest("missinng " + field + " field") ;
+            return;
+        }
+    });
+
+    if(typeof(newQuestion.answers) != typeof([])) {
+        invalidRequest("field answers must be an array");
+        return;
+    }
+
+    questions.push(newQuestion);
+    response.status(201).send(newQuestion);
 })
 
 
