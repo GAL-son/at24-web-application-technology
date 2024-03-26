48634a47e21cb7065f5e6aaf503aaedf34797aac
GAL_son <pancerek1@gmail.com>
2024-03-26 11:31:39 +0100

    Exc. 5

--- Lab5/app.js
+++ Lab5/app.js
@@ -6,8 +6,17 @@ const app = express();
 app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
 
+let chart1 = {"type":"line","data":{"labels":["January","February","March","April","May","June"],"datasets":[{"label":"My First dataset","backgroundColor":"rgb(255, 99, 132)","borderColor":"rgb(255, 99, 132)","data":[0,10,5,2,20,30,45]}]},"options":{}};
+
+
 app.get('/', (request, response) => {
-    response.render(__dirname + "/index.html", {});
+    response.render(
+        __dirname + "/index.html", 
+        { 
+            subject: 'Technologie aplikacji webowych' ,
+            chart1: JSON.stringify(chart1)
+        }
+    );
 });
 
 app.listen(config.port, () => {
